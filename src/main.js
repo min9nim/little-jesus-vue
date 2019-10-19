5c7a2e0f6548e029b3328b43d02e21649c20cbe9
min9nim <min9nim@gmail.com>
2019-10-19 23:10:00 +0900

    initial setting

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueCompositionApi from '@vue/composition-api'
 
 Vue.config.productionTip = false
+Vue.use(VueCompositionApi)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-for="(student, index) in state.students" :key="index">
+      이름: {{ student.name }}
+      <div>
+        출석여부
+      </div>
+      <div>
+        출석여부
+      </div>
+      <div>
+        말씀묵상
+      </div>
+      <div>
+        말씀암송
+      </div>
+      <div>
+        전도
+      </div>
+      <div>
+        기타
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {createComponent, reactive, onMounted} from '@vue/composition-api'
+import gql from 'graphql-tag'
+import {req} from '@/utils'
+
+export default createComponent({
+  setup() {
+    const state = reactive({
+      students: [],
+    })
+
+    onMounted(async () => {
+      const result = await req(gql`
+        {
+          students {
+            name
+          }
+        }
+      `)
+      // console.log(result)
+      state.students = result.students
+    })
+
+    return {
+      state,
+    }
+  },
+})
+</script>
+<style scoped>
+
+</style>
