f7aa90352bca0df0274feec14d8e3557c13fc05c
min9nim <min9nim@gmail.com>
2019-10-20 00:27:04 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,23 @@
 #app
   #nav
     router-link(to="/") 출석체크
-    router-link(to="/about") 포인트현황
+    router-link(to="/point") 포인트현황
   router-view
 </template>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
 #app
 	font-family 'Avenir', Helvetica, Arial, sans-serif
 	-webkit-font-smoothing antialiased
 	-moz-osx-font-smoothing grayscale
 	text-align center
 	color #2c3e50
-</style>
-
-<style lang="stylus">
 #nav
-  padding 10px
-  a
-    margin 0 10px
+	padding 10px
+	a
+		margin 0 10px
 		font-weight bold
 		color #2c3e50
 		&.router-link-exact-active 
-			color #42b983
+			color #42b983	
 </style>
