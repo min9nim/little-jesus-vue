44818c41dc5e5b389f6216fc8e547eab8aa807dd
min9nim <min9nim@gmail.com>
2019-10-31 12:36:48 +0900

    update setting of no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
-    "no-unused-vars": ["warn"],
-    "prettier/prettier": ["error", {"semi": false}],
-    "no-console": "warn",
-    "no-debugger": "warn",
-    "object-curly-spacing": "off",
+    'no-unused-vars': ['warn', { "varsIgnorePattern": "I[A-Z]*" }],
+    'prettier/prettier': ['error', {semi: false}],
+    'no-console': ['warn', {allow: ['warn', 'error']}],
+    'no-debugger': 'warn',
+    'object-curly-spacing': 'off',
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
   },
 }
