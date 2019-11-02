04bf81929c125c66043fcf0de924a100467d018a
min9nim <min9nim@gmail.com>
2019-11-03 03:37:30 +0900

    운영/개발 환경 접속 api 서버 변경

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import {print} from 'graphql/language/printer'
 
-const BASEURL = 'https://little-jesus-api.now.sh'
-const headers = {'Content-Type': 'application/json'}
+const prod_url = 'https://little-jesus-api.now.sh'
+const dev_url = 'https://little-jesus-api-git-develop.min1.now.sh'
+const BASEURL = window.location.host === 'little-jesus.now.sh' ? prod_url : dev_url
 
 export async function req(query: any, variables = {}) {
-  let config = {headers}
+  let config = {headers: {'Content-Type': 'application/json'}}
   const result = await axios.post(BASEURL, {query: print(query), variables}, config)
   if (result.data.errors) {
     throw result.data.errors
