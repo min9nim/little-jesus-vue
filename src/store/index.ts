50cde3ff717e481c393eedabe977206dbf004ed9
min9nim <min9nim@gmail.com>
2019-11-26 03:59:30 +0900

    add pointMenus state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {IPublicState, ITeacher, IPoint, IStudent} from '@/biz/type'
+import {IPointMenu, ITeacher, IPoint, IStudent} from '@/biz/type'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     teachers: [] as ITeacher[],
+    pointMenus: [] as IPointMenu[],
   },
   mutations: {
     setTeachers(state, teachers) {
       state.teachers = teachers
+    },
+    setPointMenus(state, pointMenus) {
+      state.pointMenus = pointMenus
     },
     addTeacher(state, teacher) {
       state.teachers.push(teacher)
