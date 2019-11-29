3f4f2eafd2321784160feb70a00e7a2dc808959b
min9nim <min9nim@gmail.com>
2019-11-30 07:47:52 +0900

    센트리 세팅

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 import locale from 'element-ui/lib/locale/lang/ko'
 // import {ko} from 'element-ui/locale'   // not exist
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+console.log('VUE_APP_SENTRY_DSN', process.env.VUE_APP_SENTRY_DSN)
+
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+})
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
