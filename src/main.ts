719e8cdd0d2dcae29c11c3d67d533c4d7b46989e
min9nim <min9nim@gmail.com>
2019-11-30 10:42:50 +0900

    console.log to flatLog

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,13 @@ import locale from 'element-ui/lib/locale/lang/ko'
 // import {ko} from 'element-ui/locale'   // not exist
 import * as Sentry from '@sentry/browser'
 import * as Integrations from '@sentry/integrations'
+import {flatLog} from '@mgsong/min-utils'
 
 const {
   VUE_APP_SENTRY_DSN = 'https://0900362973204fe39f4d3c815e03ec9e@sentry.io/1839663',
 } = process.env
 
-console.log({VUE_APP_SENTRY_DSN})
+flatLog('sentry-dsn:', VUE_APP_SENTRY_DSN)
 
 Sentry.init({
   dsn: VUE_APP_SENTRY_DSN,
