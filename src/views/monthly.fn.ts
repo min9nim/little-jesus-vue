5d26729b85435b16888cba4b5a1994312640f3ba
min9nim <min9nim@gmail.com>
2019-12-06 07:21:30 +0900

    qPointsFromTo 조회

--- src/views/monthly.fn.ts
+++ src/views/monthly.fn.ts
@@ -1,10 +1,16 @@
 import moment from 'moment'
+import {req} from '@/utils'
+import {qPointsFromTo} from '@/biz/query'
 export function useHandleMonthChange({root, state}: any) {
   return async (value: string) => {
     const start = value
     const end = moment(value, 'YYYYMMDD')
       .endOf('month')
       .format('YYYYMMDD')
-    console.log(start, end)
+    const result = await req(qPointsFromTo, {
+      startDate: start,
+      endDate: end,
+    })
+    console.log(result)
   }
 }
