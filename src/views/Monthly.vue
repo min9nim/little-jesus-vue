4e2e72fb2066fece63b20c45d5a5b2feb93e9e7c
min9nim <min9nim@gmail.com>
2019-12-07 22:12:37 +0900

    월별 합계 출력

--- src/views/Monthly.vue
+++ src/views/Monthly.vue
@@ -9,6 +9,13 @@
       placeholder="월 선택"
       @change="handleMonthChange"
     )
+  el-table(:data="state.tableData" style="width: 100%")
+    el-table-column(prop="name" label="이름")
+    el-table-column(prop="week1" label="11/3")
+    el-table-column(prop="week2" label="11/10")
+    el-table-column(prop="week3" label="11/17")
+    el-table-column(prop="week4" label="11/24")
+
 </template>
 
 <script lang="ts">
@@ -21,6 +28,36 @@ export default {
     const state = reactive({
       date: '',
       loading: false,
+      tableData: [
+        {
+          name: '송하연',
+          week1: 10,
+          week2: 20,
+          week3: 30,
+          week4: 40,
+        },
+        {
+          name: '송하니',
+          week1: 10,
+          week2: 20,
+          week3: 30,
+          week4: 40,
+        },
+        {
+          name: '우아름',
+          week1: 10,
+          week2: 20,
+          week3: 30,
+          week4: 40,
+        },
+        {
+          name: '우미소',
+          week1: 10,
+          week2: 20,
+          week3: 30,
+          week4: 40,
+        },
+      ],
     })
     const handleMonthChange = useHandleMonthChange({root, state})
     return {
--- src/views/monthly.fn.ts
+++ src/views/monthly.fn.ts
@@ -1,16 +1,37 @@
 import moment from 'moment'
 import {req} from '@/utils'
 import {qPointsFromTo} from '@/biz/query'
+import {groupBy, path, map} from 'ramda'
+
 export function useHandleMonthChange({root, state}: any) {
   return async (value: string) => {
     const start = value
     const end = moment(value, 'YYYYMMDD')
       .endOf('month')
       .format('YYYYMMDD')
+    state.loading = true
     const result = await req(qPointsFromTo, {
       startDate: start,
       endDate: end,
     })
+    state.loading = false
     console.log(result)
+    const points = result.res
+    const pointsByStudent = groupBy(path(['owner', 'name']) as any)(points)
+    console.log(pointsByStudent)
+    state.tableData = Object.entries(pointsByStudent).map(([key, value]: any) => {
+      console.log({key, value})
+      return {
+        name: key,
+        week1: value[0] ? getPointSumOfWeek(value[0]) : 0,
+        week2: value[1] ? getPointSumOfWeek(value[1]) : 0,
+        week3: value[2] ? getPointSumOfWeek(value[2]) : 0,
+        week4: value[3] ? getPointSumOfWeek(value[3]) : 0,
+      }
+    })
   }
+}
+
+function getPointSumOfWeek(point: any) {
+  return point.items.reduce((acc: number, item: any) => acc + item.type.priority * item.value, 0)
 }
