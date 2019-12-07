98d3b0e352b3490fff7b210e366275a9732fcaea
min9nim <min9nim@gmail.com>
2019-12-08 01:33:25 +0900

    new getSundaysOfMonth

--- src/views/monthly.fn.ts
+++ src/views/monthly.fn.ts
@@ -35,3 +35,42 @@ export function useHandleMonthChange({root, state}: any) {
 function getPointSumOfWeek(point: any) {
   return point.items.reduce((acc: number, item: any) => acc + item.type.priority * item.value, 0)
 }
+
+function getSundaysOfMonth(yearMonth: string) {
+  const month = yearMonth.slice(-2)
+  const result = []
+  const sunday1 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .format('MM/DD')
+  if (sunday1.slice(0, 2) === month) {
+    result.push(sunday1)
+  }
+  const sunday2 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .add(1, 'week')
+    .format('MM/DD')
+  const sunday3 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .add(2, 'week')
+    .format('MM/DD')
+  const sunday4 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .add(3, 'week')
+    .format('MM/DD')
+  result.push(sunday2, sunday3, sunday4)
+  const sunday5 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .add(4, 'week')
+    .format('MM/DD')
+  if (sunday5.slice(0, 2) === month) {
+    result.push(sunday5)
+  }
+  const sunday6 = moment(yearMonth, 'YYYYMM')
+    .startOf('week')
+    .add(5, 'week')
+    .format('MM/DD')
+  if (sunday6.slice(0, 2) === month) {
+    result.push(sunday6)
+  }
+  return result
+}
