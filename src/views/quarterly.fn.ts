7e8ef89d82aed69c982cda72fc3907ef65838164
min9nim <min9nim@gmail.com>
2019-12-08 05:02:46 +0900

    분기별합계 화면 구현

---
+++ src/views/quarterly.fn.ts
@@ -0,0 +1,85 @@
+import moment from 'moment'
+import {req} from '@/utils'
+import {qPointsFromTo} from '@/biz/query'
+import {groupBy, path, propEq} from 'ramda'
+import {getPointSumOfWeek} from './monthly.fn'
+
+export function useHandleQuarterChange({state}: any) {
+  return async (value: number) => {
+    const startMonth = (value - 1) * 3 + 1
+    const start = moment(state.year + String(startMonth).padStart(2, '0'), 'YYYYMM').format(
+      'YYYYMMDD',
+    )
+    const end = moment(start, 'YYYYMMDD')
+      .endOf('month')
+      .add(2, 'month')
+      .format('YYYYMMDD')
+    state.loading = true
+    const result = await req(qPointsFromTo, {
+      startDate: start,
+      endDate: end,
+    })
+    state.loading = false
+    const points = result.res
+    const pointsByStudent = groupBy(path(['owner', 'name']) as any)(points)
+    // console.log(pointsByStudent)
+    state.tableData = getTableData({pointsByStudent, quarter: value})
+  }
+}
+
+export function getTableData({pointsByStudent, quarter}: any) {
+  const months = getMonthsOfQuarter(quarter)
+  // console.log('*** months: ', months)
+  return Object.entries(pointsByStudent).map(([name, points]: any) => {
+    // console.log(points)
+    const teacher = points[0].owner.teacher
+    const teacherName = teacher ? teacher.name : 'N/A'
+    const pointsByMonth = groupBy((point: any) => {
+      return point.date.substr(4, 2)
+    })(points)
+    // console.log('pointsByMonth', pointsByMonth)
+
+    const sumByMonth: any = {}
+    let totalSum = 0
+    months.forEach((month, index) => {
+      // console.log(44, pointsByMonth[month])
+      sumByMonth['month' + (index + 1)] = pointsByMonth[month]
+        ? pointsByMonth[month].reduce((acc, point) => acc + getPointSumOfWeek(point), 0)
+        : 0
+      totalSum += sumByMonth['month' + (index + 1)]
+    })
+
+    // console.log(sumByMonth)
+
+    return {
+      name,
+      teacher: teacherName,
+      ...sumByMonth,
+      totalSum,
+    }
+  })
+}
+
+export function getMonthsOfQuarter(quarter: number) {
+  const start = (quarter - 1) * 3 + 1
+  return [start, start + 1, start + 2]
+}
+
+export const quarterOptions = [
+  {
+    value: 1,
+    label: '1분기',
+  },
+  {
+    value: 2,
+    label: '2분기',
+  },
+  {
+    value: 3,
+    label: '3분기',
+  },
+  {
+    value: 4,
+    label: '4분기',
+  },
+]
