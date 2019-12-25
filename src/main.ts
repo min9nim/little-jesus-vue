b9e0d76879c888c10c50864714d7e5f646358708
min9nim <min9nim@gmail.com>
2019-12-26 07:55:29 +0900

    로그위치 매핑이 안되서 sentry 설정 제거

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,20 @@ import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 import locale from 'element-ui/lib/locale/lang/ko'
 // import {ko} from 'element-ui/locale'   // not exist
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations'
-import {flatLog} from '@mgsong/min-utils'
+// import * as Sentry from '@sentry/browser'
+// import * as Integrations from '@sentry/integrations'
+// import {flatLog} from '@mgsong/min-utils'
 
-const {
-  VUE_APP_SENTRY_DSN = 'https://0900362973204fe39f4d3c815e03ec9e@sentry.io/1839663',
-} = process.env
+// const {
+//   VUE_APP_SENTRY_DSN = 'https://0900362973204fe39f4d3c815e03ec9e@sentry.io/1839663',
+// } = process.env
 
-flatLog('sentry-dsn:', VUE_APP_SENTRY_DSN)
+// flatLog('sentry-dsn:', VUE_APP_SENTRY_DSN)
 
-Sentry.init({
-  dsn: VUE_APP_SENTRY_DSN,
-  integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
-})
+// Sentry.init({
+//   dsn: VUE_APP_SENTRY_DSN,
+//   integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
+// })
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
