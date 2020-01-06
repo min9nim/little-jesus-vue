ec96552fc5e40b52be95235167636e011d369a11
min9nim <min9nim@gmail.com>
2020-01-07 08:06:00 +0900

    테스트케이스 작업 중

---
+++ test/src/views/point.fn.spec.ts
@@ -0,0 +1,10 @@
+import {expect} from 'chai'
+import {getEtcStudentPoints} from '../../../src/views/points.fn'
+
+describe('point.fn.ts test', () => {
+  describe('getEtcStudentPoints', () => {
+    it('should return etcStudentPoints', () => {
+      // getEtcStudentPoints()
+    })
+  })
+})
--- test/utils/index.spec.ts
+++ test/utils/index.spec.ts
@@ -1,23 +1,17 @@
-
 import {expect} from 'chai'
 import {sort} from 'ramda'
-import {nameAscending} from '@/utils'
+import {nameAscending} from '../../src/utils'
 
 describe('utils', () => {
   describe('nameAscending', () => {
     it('should be array sorted name ascending', () => {
-      const arr = [
+      const arr = [{name: 'egh'}, {name: 'fwedf'}, {name: 'newf'}, {name: 'agb'}]
+      const sorted = sort(nameAscending)(arr)
+      expect(sorted).to.be.deep.equal([
+        {name: 'agb'},
         {name: 'egh'},
         {name: 'fwedf'},
         {name: 'newf'},
-        {name: 'agb'},
-      ]
-      const sorted = sort(nameAscending)(arr)
-      expect(sorted).to.be.deep.equal([
-        { name: 'agb' },
-        { name: 'egh' },
-        { name: 'fwedf' },
-        { name: 'newf' }
       ])
     })
   })
