d8331ad56bc40941ce6b2a49e3b53e3535d97160
min9nim <min9nim@gmail.com>
2020-01-07 10:03:47 +0900

    resolve #17

--- test/src/views/point.fn.spec.ts
+++ test/src/views/point.fn.spec.ts
@@ -1,10 +1,17 @@
 import {expect} from 'chai'
 import {getEtcStudentPoints} from '../../../src/views/points.fn'
+import {studentToDefaultPointMap} from '../../../src/biz'
+import {points, etcStudents, etcStudentPoint, pointMenus} from '../../mock/data'
 
 describe('point.fn.ts test', () => {
   describe('getEtcStudentPoints', () => {
     it('should return etcStudentPoints', () => {
-      // getEtcStudentPoints()
+      const result = getEtcStudentPoints({
+        points,
+        etcStudents,
+        defaultPoint: studentToDefaultPointMap(pointMenus),
+      })
+      expect(result).to.be.deep.equal(etcStudentPoint)
     })
   })
 })
