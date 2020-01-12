8917c739f9f964474e0a0825c1a87a4ec04673d7
min9nim <min9nim@gmail.com>
2020-01-12 17:19:53 +0900

    reformat

--- src/views/Points.vue
+++ src/views/Points.vue
@@ -15,14 +15,22 @@
         .title(slot="header")
           h3.teacher {{teacherName}}
           router-link(to="/?edit")
-            el-button.btn(size="mini" icon="el-icon-edit" @click="handleClick(teacherName)") {{points.length ? '수정' : '입력'}}
+            el-button.btn(
+              size="mini"
+              icon="el-icon-edit"
+              @click="handleClick(teacherName)"
+            ) {{points.length ? '수정' : '입력'}}
         table-point(:points="points")
     .result(v-if="state.pointsByTeacher && state.etcStudents.length > 0")
       el-card(shadow="hover")
         .title(slot="header")
           h3.teacher 반미정
           router-link(to="/?edit")
-            el-button.btn(size="mini" icon="el-icon-edit" @click="handleClick('반미정')") {{state.pointsByTeacher['반미정'].length ? '수정' : '입력'}}
+            el-button.btn(
+              size="mini"
+              icon="el-icon-edit"
+              @click="handleClick('반미정')"
+            ) {{state.pointsByTeacher['반미정'].length ? '수정' : '입력'}}
         table-point(:points="state.pointsByTeacher['반미정']")      
   hr
   .sum
