8058f534dce544f9ec650c9a9f7d6fd597e623a5
min9nim <min9nim@gmail.com>
2020-02-23 07:58:26 +0900

    use initSentry of lj-common

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
 import locale from 'element-ui/lib/locale/lang/ko'
 // import {ko} from 'element-ui/locale'   // not exist
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations'
-import {flatLog} from 'mingutils'
-import {setApiServer} from '@/utils'
-
-if (window.location.host === 'little-jesus.now.sh') {
-  const {
-    VUE_APP_SENTRY_DSN = 'https://0900362973204fe39f4d3c815e03ec9e@sentry.io/1839663',
-  } = process.env
-  flatLog('sentry-dsn:', VUE_APP_SENTRY_DSN)
-  Sentry.init({
-    dsn: VUE_APP_SENTRY_DSN,
-    integrations: [new Integrations.Vue({Vue, attachProps: true, logErrors: true})],
-  })
-}
+import {setApiServer, initSentry} from '@/utils'
 
 setApiServer()
+initSentry(Vue)
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
