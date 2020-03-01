19a744c687a89710d7a16a02e2eae27ae219c414
min9nim <min9nim@gmail.com>
2020-03-02 08:00:41 +0900

    add comment

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import router from './router'
 import VueCompositionApi from '@vue/composition-api'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// import Tag from 'element-ui/lib/tag'
+// import Input from 'element-ui/lib/input'
+// import Tabs from 'element-ui/lib/tabs'
+// import TabPane from 'element-ui/lib/tab-pane'
+
 import store from './store'
 import locale from 'element-ui/lib/locale/lang/ko'
-// import {ko} from 'element-ui/locale'   // not exist
+// import {ko} from 'element-ui/locale' // not exist
 import {setApiServer, initSentry} from '@/utils'
 
 setApiServer()
@@ -15,6 +20,10 @@ initSentry(Vue)
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
 Vue.use(ElementUI, {locale})
+// Vue.use(Input)
+// Vue.use(Tabs)
+// Vue.use(TabPane)
+// Vue.use(Tag)
 
 new Vue({
   router,
